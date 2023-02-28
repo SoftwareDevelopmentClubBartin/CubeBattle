bf6ce6dc3071515764c080c71e1a9a7baf84b506
muhammet484 <bjk_484@hotmail.com>
2023-02-28 16:29:47 +0300

    Upgraded engine version

--- Assets/Main Game/Scripts/PlayerManager.cs
+++ Assets/Main Game/Scripts/PlayerManager.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+    Rigidbody2D rigidbody; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var a = new Vector2(5.5f, 5.6f);
+        float YanalHareket = Input.GetAxisRaw("Horizontal");
+        float DikeyHareket = Input.GetAxisRaw("Vertical");
     }
 }
